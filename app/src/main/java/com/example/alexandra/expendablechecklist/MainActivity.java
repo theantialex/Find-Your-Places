57b5d9e5c61c580fdf7b381d7ac6835bb06b08c5
Alexandera Volynets <saxara21@gmail.com>
2018-12-14 18:40:49 +0300

    dgzj

---
+++ app/src/main/java/com/example/alexandra/expendablechecklist/MainActivity.java
@@ -0,0 +1,160 @@
+package com.example.alexandra.expendablechecklist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import com.example.alexandra.expendablechecklist.Model.DataItem;
+import com.example.alexandra.expendablechecklist.Model.SubCategoryItem;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private ExpandableListView lvCategory;
+
+    private ArrayList<DataItem> arCategory;
+    private ArrayList<SubCategoryItem> arSubCategory;
+    private ArrayList<ArrayList<SubCategoryItem>> arSubCategoryFinal;
+
+    private ArrayList<HashMap<String, String>> parentItems;
+    private ArrayList<ArrayList<HashMap<String, String>>> childItems;
+    private MyCategoriesExpandableListAdapter myCategoriesExpandableListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CheckedActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        setupReferences();
+    }
+
+    private void setupReferences() {
+
+        lvCategory = findViewById(R.id.lvCategory);
+        arCategory = new ArrayList<>();
+        arSubCategory = new ArrayList<>();
+        parentItems = new ArrayList<>();
+        childItems = new ArrayList<>();
+
+        DataItem dataItem = new DataItem();
+        dataItem.setCategoryId("1");
+        dataItem.setCategoryName("Активный отдых");
+
+        arSubCategory = new ArrayList<>();
+        ArrayList<String> ActTypes = new ArrayList<>();
+        ActTypes.add("Парк");
+        ActTypes.add("Зоопарк");
+        ActTypes.add("Парк аттракционов");
+        ActTypes.add("Океанариум");
+        for (int i = 0; i < ActTypes.size(); i++) {
+
+            SubCategoryItem subCategoryItem = new SubCategoryItem();
+            subCategoryItem.setCategoryId(String.valueOf(i));
+            subCategoryItem.setIsChecked(ConstantManager.CHECK_BOX_CHECKED_FALSE);
+            subCategoryItem.setSubCategoryName(ActTypes.get(i));
+            arSubCategory.add(subCategoryItem);
+        }
+        dataItem.setSubCategory(arSubCategory);
+        arCategory.add(dataItem);
+
+        dataItem = new DataItem();
+        dataItem.setCategoryId("2");
+        dataItem.setCategoryName("Культурный отдых");
+        arSubCategory = new ArrayList<>();
+        ArrayList<String> CulTypes = new ArrayList<>();
+        CulTypes.add("Музей");
+        CulTypes.add("Картинная галерея");
+        CulTypes.add("Церковь");
+        CulTypes.add("Мечеть");
+        CulTypes.add("Синанога");
+        CulTypes.add("Библиотека");
+        CulTypes.add("Индейский храм");
+        for (int j = 0; j < CulTypes.size(); j++) {
+
+            SubCategoryItem subCategoryItem = new SubCategoryItem();
+            subCategoryItem.setCategoryId(String.valueOf(j));
+            subCategoryItem.setIsChecked(ConstantManager.CHECK_BOX_CHECKED_FALSE);
+            subCategoryItem.setSubCategoryName(CulTypes.get(j));
+            arSubCategory.add(subCategoryItem);
+        }
+        dataItem.setSubCategory(arSubCategory);
+        arCategory.add(dataItem);
+
+        dataItem = new DataItem();
+        dataItem.setCategoryId("3");
+        dataItem.setCategoryName("Еда");
+        arSubCategory = new ArrayList<>();
+
+        for (int k = 1; k < 6; k++) {
+
+            SubCategoryItem subCategoryItem = new SubCategoryItem();
+            subCategoryItem.setCategoryId(String.valueOf(ActTypes));
+            subCategoryItem.setIsChecked(ConstantManager.CHECK_BOX_CHECKED_FALSE);
+            subCategoryItem.setSubCategoryName("Cooking: " + k);
+            arSubCategory.add(subCategoryItem);
+        }
+
+        dataItem.setSubCategory(arSubCategory);
+        arCategory.add(dataItem);
+
+        Log.d("TAG", "setupReferences: " + arCategory.size());
+
+        for (DataItem data : arCategory) {
+//                        Log.i("Item id",item.id);
+            ArrayList<HashMap<String, String>> childArrayList = new ArrayList<HashMap<String, String>>();
+            HashMap<String, String> mapParent = new HashMap<String, String>();
+
+            mapParent.put(ConstantManager.Parameter.CATEGORY_ID, data.getCategoryId());
+            mapParent.put(ConstantManager.Parameter.CATEGORY_NAME, data.getCategoryName());
+
+            int countIsChecked = 0;
+            for (SubCategoryItem subCategoryItem : data.getSubCategory()) {
+
+                HashMap<String, String> mapChild = new HashMap<String, String>();
+                mapChild.put(ConstantManager.Parameter.SUB_ID, subCategoryItem.getSubId());
+                mapChild.put(ConstantManager.Parameter.SUB_CATEGORY_NAME, subCategoryItem.getSubCategoryName());
+                mapChild.put(ConstantManager.Parameter.CATEGORY_ID, subCategoryItem.getCategoryId());
+                mapChild.put(ConstantManager.Parameter.IS_CHECKED, subCategoryItem.getIsChecked());
+
+                if (subCategoryItem.getIsChecked().equalsIgnoreCase(ConstantManager.CHECK_BOX_CHECKED_TRUE)) {
+
+                    countIsChecked++;
+                }
+                childArrayList.add(mapChild);
+            }
+
+            if (countIsChecked == data.getSubCategory().size()) {
+
+                data.setIsChecked(ConstantManager.CHECK_BOX_CHECKED_TRUE);
+            } else {
+                data.setIsChecked(ConstantManager.CHECK_BOX_CHECKED_FALSE);
+            }
+
+            mapParent.put(ConstantManager.Parameter.IS_CHECKED, data.getIsChecked());
+            childItems.add(childArrayList);
+            parentItems.add(mapParent);
+
+        }
+
+        ConstantManager.parentItems = parentItems;
+        ConstantManager.childItems = childItems;
+
+        myCategoriesExpandableListAdapter = new MyCategoriesExpandableListAdapter(this, parentItems, childItems, false);
+        lvCategory.setAdapter(myCategoriesExpandableListAdapter);
+    }
+}
