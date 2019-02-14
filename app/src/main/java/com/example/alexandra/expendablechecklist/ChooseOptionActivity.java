1e5f4a4059d079f8b6f4dee8119253509f6f44e4
theantialex <saxara21@gmail.com>
2019-02-14 18:11:25 +0300

    Finishline

--- app/src/main/java/com/example/alexandra/expendablechecklist/ChooseOptionActivity.java
+++ app/src/main/java/com/example/alexandra/expendablechecklist/ChooseOptionActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ChooseOptionActivity extends AppCompatActivity {
     private Button act_new;
+    private Button act_old;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class ChooseOptionActivity extends AppCompatActivity {
     public void addListenerOnButton() {
 
         act_new = (Button) findViewById(R.id.btnNewRoute);
+        act_old = findViewById(R.id.btnFavRoute);
 
         act_new.setOnClickListener(
                 new View.OnClickListener() {
@@ -30,6 +33,16 @@ public class ChooseOptionActivity extends AppCompatActivity {
                         startActivity(intent);
 
 
+                    }
+                }
+        );
+        act_old.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "Эта функция находится в разработке", Toast.LENGTH_SHORT);
+                        toast.show();
                     }
                 }
         );
